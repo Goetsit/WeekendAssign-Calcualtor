f5d5a14ccc4cf1ef438b2d04372d413c67ab0df9
Laura Goetz <30483069+Goetsit@users.noreply.github.com>
2017-10-13 16:27:43 -0500

    so far so good, added to server side code

--- server.js
+++ server.js
@@ -1,3 +1,18 @@
 var express = require('express');
 var app = express();
-var bodyparse = require('body-parser');
+var bodyParser = require('body-parser');
+var port = 5000;
+
+app.use(express.static('server/public'));
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.post('/calculate',function(req,res){
+   var firstNumber = parseInt(req.body.firstNumber);
+   var secondNumber = parseInt(req.body.secondNumber);
+
+   console.log('numbers from server', firstNumber, secondNumber);
+})
+
+app.listen(port, function(){
+    console.log('listening on port', port);
+})
