824a0d77b95f47b912db555a4811a88b2d635d3d
Laura Goetz <30483069+Goetsit@users.noreply.github.com>
2017-10-13 16:28:10 -0500

    added $.ajax and POST

--- scripts/client.js
+++ scripts/client.js
@@ -12,7 +12,14 @@ function addNumbers(){
     var $numbersToCalc = {firstNumber: $('#firstNumber').val(),
                 secondNumber: $('#secondNumber').val()
             };
-    
-
+    $.ajax({
+        method:'POST',
+        url: '/calculate',
+        data: $numbersToCalc
+    }).done(function(response){
+        console.log('I guess things worked', $numbersToCalc);
+    }).fail(function(message){
+        console.log('things broke', $numbersToCalc);
+    })
 console.log($numbersToCalc);
 }
