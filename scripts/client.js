2a3585872e2d8204e31df16b9d0b622fb5ed44dd
Laura Goetz <30483069+Goetsit@users.noreply.github.com>
2017-10-13 19:23:31 -0500

    numbers successfully doing the things, add - done kinda

--- scripts/client.js
+++ scripts/client.js
@@ -9,17 +9,68 @@ function readyNow(){
 
 
 function addNumbers(){
-    var $numbersToCalc = {firstNumber: $('#firstNumber').val(),
-                secondNumber: $('#secondNumber').val()
-            };
+    var first = parseInt($('#firstNumber').val());
+    var second = parseInt($('#secondNumber').val());
+
+
     $.ajax({
         method:'POST',
-        url: '/calculate',
-        data: $numbersToCalc
+        url: '/add',
+        data: {
+            first,
+            second
+        }
     }).done(function(response){
-        console.log('I guess things worked', $numbersToCalc);
+        console.log('I guess things worked', first,second);
+
+        getNumbers();
     }).fail(function(message){
-        console.log('things broke', $numbersToCalc);
+        console.log('things broke', first,second);
     })
-console.log($numbersToCalc);
+console.log(first,second);
+
+
+function getNumbers(){
+    $.ajax({
+       method:'GET',
+       url: '/add'
+   }).done(function(response){
+       var numbersAdded = response;
+
+       console.log('GET returned these numbers',numbersAdded);
+       console.log('addedNumber variable',numbersAdded);
+
+       appendToDOM(numbersAdded);
+
+    }).fail(function(message){
+        console.log('did not work?');
+    })
+
+    }
+}
+
+function appendToDOM(numbers){
+
+    /*
+    console.log('beginning of append',numbers);
+    
+    $('#testspan').html(numbers.number);
+    */
+
+    //$('#numbersTable').empty();
+        for(var i = 0; i < numbers.length; i += 1) {
+            // Append to the dom
+            var numberTest = numbers[i];
+
+            $('#testspan').html(numberTest);
+            console.log(numberTest);
+            /*
+            var $tr = $('<tr></tr>');
+            $tr.append('<td>' + numberTest + '</td>');
+            $('#numbersTable').append($tr);
+
+            */
+        }
+
+        
 }
