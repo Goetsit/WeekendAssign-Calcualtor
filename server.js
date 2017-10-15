1d1a9fe05afc7c7066ac426eac462c4481c1e6a7
Laura Goetz <30483069+Goetsit@users.noreply.github.com>
2017-10-15 10:40:35 -0500

    added .post and .get for other operators

--- server.js
+++ server.js
@@ -8,13 +8,13 @@ app.use(bodyParser.urlencoded({extended:true}));
 
 var numberArray = [];
 
+/*BEGIN ADD*/ 
 app.post('/add',function(req,res){
    
    var firstNumber = parseInt(req.body.first);
    var secondNumber = parseInt(req.body.second);
     numberArray.push(firstNumber+secondNumber);
 console.log('testerMctestTest',numberArray);
-   
    
    res.send(201);
 })
@@ -23,6 +23,57 @@ app.get('/add',function(req,res){
     console.log('from GET',numberArray)
     res.send(numberArray);
 })
+/*END ADD*/
+/*BEGIN SUBTRACT*/
+app.post('/subtract',function(req,res){
+    
+    var firstNumber = parseInt(req.body.first);
+    var secondNumber = parseInt(req.body.second);
+     numberArray.push(firstNumber-secondNumber);
+ console.log('testerMctestTest',numberArray);
+    
+    res.send(201);
+ })
+ 
+ app.get('/subtract',function(req,res){
+     console.log('from GET',numberArray)
+     res.send(numberArray);
+ })
+ 
+/*END SUBTRACT*/
+/*BEGIN DIVIDE*/ 
+app.post('/divide',function(req,res){
+    
+    var firstNumber = parseInt(req.body.first);
+    var secondNumber = parseInt(req.body.second);
+     numberArray.push(firstNumber/secondNumber);
+ console.log('testerMctestTest',numberArray);
+    
+    res.send(201);
+ })
+ 
+ app.get('/divide',function(req,res){
+     console.log('from GET',numberArray)
+     res.send(numberArray);
+ })
+/* END DIVIDE*/
+
+/*BEGIN MULTIPLY*/
+app.post('/multiply',function(req,res){
+    
+    var firstNumber = parseInt(req.body.first);
+    var secondNumber = parseInt(req.body.second);
+     numberArray.push(firstNumber*secondNumber);
+ console.log('testerMctestTest',numberArray);
+    
+    res.send(201);
+ })
+ 
+ app.get('/multiply',function(req,res){
+     console.log('from GET',numberArray)
+     res.send(numberArray);
+ })
+/*END MULTIPLY*/
 
 app.listen(port, function(){
     console.log('listening on port', port);
