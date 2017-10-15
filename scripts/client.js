02212cc4ee59e8f54b5dd4184f9ddbfd17d8f21d
Laura Goetz <30483069+Goetsit@users.noreply.github.com>
2017-10-15 10:41:17 -0500

    added clearall and other operators

--- scripts/client.js
+++ scripts/client.js
@@ -5,9 +5,13 @@ $(document).ready(readyNow);
 function readyNow(){
     console.log('JQ sourced');
     $('#addBtn').on('click', addNumbers);
+    $('#subtractBtn').on('click', subtractNumbers)
+    $('#divideBtn').on('click', divideNumbers);
+    $('#multiplyBtn').on('click', multiplyNumbers);
+    $('#clear').on('click', clearAll);
 }//click hanlders
 
-
+/*BEGIN ADD*/ 
 function addNumbers(){
     var first = parseInt($('#firstNumber').val());
     var second = parseInt($('#secondNumber').val());
@@ -23,14 +27,14 @@ function addNumbers(){
     }).done(function(response){
         console.log('I guess things worked', first,second);
 
-        getNumbers();
+        getNumbersAdd();
     }).fail(function(message){
         console.log('things broke', first,second);
     })
 console.log(first,second);
 
 
-function getNumbers(){
+function getNumbersAdd(){
     $.ajax({
        method:'GET',
        url: '/add'
@@ -49,28 +53,158 @@ function getNumbers(){
     }
 }
 
+/*END ADD*/
+/*BEGIN SUBTRACT*/
+function subtractNumbers(){
+    var first = parseInt($('#firstNumber').val());
+    var second = parseInt($('#secondNumber').val());
+
+
+    $.ajax({
+        method:'POST',
+        url: '/subtract',
+        data: {
+            first,
+            second
+        }
+    }).done(function(response){
+        console.log('I guess things worked', first,second);
+
+        getNumbersSubtract();
+    }).fail(function(message){
+        console.log('things broke', first,second);
+    })
+console.log(first,second);
+
+
+function getNumbersSubtract(){
+    $.ajax({
+       method:'GET',
+       url: '/subtract'
+   }).done(function(response){
+       var numbersSubtracted = response;
+
+       console.log('GET returned these numbers',numbersSubtracted);
+       console.log(' variable',numbersSubtracted);
+
+       appendToDOM(numbersSubtracted);
+
+    }).fail(function(message){
+        console.log('did not work?');
+    })
+
+    }
+}
+/*END SUBTRACT*/
+
+/* BEGIN DIVIDE */
+function divideNumbers(){
+    var first = parseInt($('#firstNumber').val());
+    var second = parseInt($('#secondNumber').val());
+
+
+    $.ajax({
+        method:'POST',
+        url: '/divide',
+        data: {
+            first,
+            second
+        }
+    }).done(function(response){
+        console.log('I guess things worked', first,second);
+
+        getNumbersDivide();
+    }).fail(function(message){
+        console.log('things broke', first,second);
+    })
+console.log(first,second);
+
+
+function getNumbersDivide(){
+    $.ajax({
+       method:'GET',
+       url: '/divide'
+   }).done(function(response){
+       var numbersDivided = response;
+
+       console.log('GET returned these numbers',numbersDivided);
+       console.log('addedNumber variable',numbersDivided);
+
+       appendToDOM(numbersDivided);
+
+    }).fail(function(message){
+        console.log('did not work?');
+    })
+
+    }
+}
+
+ /*END DIVIDE */
+/* BEGIN MULTIPLY*/
+
+function multiplyNumbers(){
+    var first = parseInt($('#firstNumber').val());
+    var second = parseInt($('#secondNumber').val());
+
+
+    $.ajax({
+        method:'POST',
+        url: '/multiply',
+        data: {
+            first,
+            second
+        }
+    }).done(function(response){
+        console.log('I guess things worked', first,second);
+
+        getNumbersMultiplied();
+    }).fail(function(message){
+        console.log('things broke', first,second);
+    })
+console.log(first,second);
+
+
+function getNumbersMultiplied(){
+    $.ajax({
+       method:'GET',
+       url: '/multiply'
+   }).done(function(response){
+       var numbersMultiplied = response;
+
+       console.log('GET returned these numbers',numbersMultiplied);
+       console.log('addedNumber variable',numbersMultiplied);
+
+       appendToDOM(numbersMultiplied);
+
+    }).fail(function(message){
+        console.log('did not work?');
+    })
+
+    }
+}
+
+/*END MULTIPLY*/
+
+/* BEGIN appendToDOM */
+
 function appendToDOM(numbers){
 
-    /*
-    console.log('beginning of append',numbers);
-    
-    $('#testspan').html(numbers.number);
-    */
-
-    //$('#numbersTable').empty();
         for(var i = 0; i < numbers.length; i += 1) {
             // Append to the dom
             var numberTest = numbers[i];
 
-            $('#testspan').html(numberTest);
+            $('#span').html(numberTest);
             console.log(numberTest);
-            /*
-            var $tr = $('<tr></tr>');
-            $tr.append('<td>' + numberTest + '</td>');
-            $('#numbersTable').append($tr);
-
-            */
+            
         }
-
-        
+       
 }
+/*END APPEND*/ 
+/* BEGIN CLEAR ALL */
+
+function clearAll(){
+    $('.numberInput').val('');
+    $('#span').html(0);
+}
+
+/*END CLEARALL*/
